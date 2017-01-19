41691f35a11ffb9fa8f2ca642c9678ace5c20a07
davit.ece.vt.edu <davit@davit.ece.vt.edu>
2017-01-19 20:57:30 +0000

    Removed spaces

--- codebase/superdarn/src.lib/tk/fitacf.2.5/src/badlags_s.c
+++ codebase/superdarn/src.lib/tk/fitacf.2.5/src/badlags_s.c
@@ -6,26 +6,26 @@
 
 /*
  LICENSE AND DISCLAIMER
- 
+
  Copyright (c) 2012 The Johns Hopkins University/Applied Physics Laboratory
- 
+
  This file is part of the Radar Software Toolkit (RST).
- 
+
  RST is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  any later version.
- 
+
  RST is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with RST.  If not, see <http://www.gnu.org/licenses/>.
- 
- 
- 
+
+
+
 */
 
 
@@ -46,9 +46,9 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
     int i, k, l, n, sample;
     long ts, t1=0, t2=0;
     int nbad;
-    
+
     int offset;
-    
+
     if (badtmp==NULL) {
     badtmp = malloc(maxbad*sizeof(int));
     if ( badtmp==NULL) {
@@ -56,16 +56,16 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         return;
     }
     }
-    
+
     i = -1;
     ts = (long) ptr->lagfr;
     sample = 0;
     k = 0;
-    
+
     t2 = 0L;
-    
+
     /* the loops below assume that smsep is not zero...this is not always the case */
-    if ( ptr->smsep <= 0 ) 
+    if ( ptr->smsep <= 0 )
     {
            /* First lets do a check to see if txpl is valid so that we can use that in place of smsep */
            if ( ptr->txpl <= 0)
@@ -76,31 +76,31 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
            /* If txpl is a valid value, lets set it as smsep and throw off a warning */
            fprintf( stderr, "FitACFBadlagsStereo: WARNING using txpl instead of smsep...\n");
            ptr->smsep = ptr->txpl;
-    }   
+    }
 
     while (i < (ptr->mppul - 1)) {
     /* first, skip over any pulses that occur before the first sample */
-    
+
     while ((ts > t2) && (i < (ptr->mppul - 1))) {
         i++;
         t1 = (long) (ptr->pulse[i]) * (long) (ptr->mpinc)
         - ptr->txpl/2;
         t2 = t1 + 3*ptr->txpl/2 + 100; /* adjust for rx-on delay */
-    }   
-    
+    }
+
     /*  we now have a pulse that occurs after the current sample.  Start
         incrementing the sample number until we find a sample that lies
         within the pulse */
-    
+
     while (ts < t1) {
         sample++;
         ts += ptr->smsep;
     }
-    
+
     /* ok, we now have a sample which occurs after the pulse starts.
        check to see if it occurs before the pulse ends, and if so, mark
         it as a bad sample */
-    
+
     while ((ts >= t1) && (ts <= t2)) {
         if ( k > maxbad ) { /* run out of storage, double the allocation */
         maxbad += MAXBAD;
@@ -128,7 +128,7 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
 
     t2 = 0L;
 
-    /* offset is the offset in time between the transmission of 
+    /* offset is the offset in time between the transmission of
        the two halves of stereo */
 
     offset=ptr->offset;
@@ -145,7 +145,7 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         t1 = (long) (ptr->pulse[i]) * (long) (ptr->mpinc)
                 - ptr->txpl/2 + offset;
         t2 = t1 + 3*ptr->txpl/2 + 100; /* adjust for rx-on delay */
-    }   
+    }
 
     /*   we now have a pulse that occurs after the current sample.  Start
          incrementing the sample number until we find a sample that lies
@@ -155,7 +155,7 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         sample++;
         ts += ptr->smsep;
     }
-    
+
     /*  ok, we now have a sample which occurs after the pulse starts.
         check to see if it occurs before the pulse ends, and if so, mark
         it as a bad sample */
@@ -176,9 +176,9 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
     }
     }
 
-   
+
     nbad = k;   /* total number of bad samples */
-    
+
 
 
     /* merge the two sets of badlags, sorting and removing duplicate entries */
@@ -196,7 +196,7 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         if ( badtmp[i] == bptr->badsmp[k] )    /* duplicate, reject it */
         break;
 
-        if ( badtmp[i] > bptr->badsmp[k] ) {  
+        if ( badtmp[i] > bptr->badsmp[k] ) {
                 /* put it in the list after the current entry */
         for ( l = n; l > k+1; l-- )
             bptr->badsmp[l] = bptr->badsmp[l-1];
@@ -205,8 +205,8 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         break;
         }
 
-        if ( k == 0 ) {     
-            /* must be less than all entries, 
+        if ( k == 0 ) {
+            /* must be less than all entries,
                put it in the list at the start */
         for ( l = n; l > 0; l-- )
             bptr->badsmp[l] = bptr->badsmp[l-1];
@@ -214,7 +214,7 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
         n++;
         }
     }
-    
+
     }
 
     bptr->nbad = n;
@@ -223,3 +223,4 @@ void FitACFBadlagsStereo(struct FitPrm *ptr, struct FitACFBadSample *bptr) {
     r_overlap(ptr);
     return;
 }
+
